921a0fcaf60e1f77a22e933caf795726a8a4d6fa
Popuri Nithin <94154780+nithin-popuri7@users.noreply.github.com>
2024-07-18 08:22:35 +0530

    Add files via upload

---
+++ D5/Script.css
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function appendCharacter(char) {
+    display.innerText += char;
+}
+
+function clearDisplay() {
+    display.innerText = '';
+}
+
+function deleteLast() {
+    display.innerText = display.innerText.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
